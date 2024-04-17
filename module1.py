14c9af30ecf66bca0e3c07cb4fa1e5f6feeabe2c
0xvasq <jorge.vasquez007@mymdc.net>
2024-04-17 12:11:29 -0400

    Fix indentation so game logic is within the while loop

--- module1.py
+++ module1.py
@@ -16,24 +16,51 @@ while True:
     except ValueError as e:
         print("ERROR, ", e)
         continue
-    else:
-        break
+    
 
-if userMove == 'r':
-    print('You have selected Rock!')
-elif userMove == 'p':
-    print ('You have selected paper!')
-elif userMove == 's':
-    print('You have selected Scissora!')
+    if userMove == 'r':
+        print('You have selected Rock!')
+    elif userMove == 'p':
+        print ('You have selected paper!')
+    elif userMove == 's':
+        print('You have selected Scissors!')
 
-randomNum = random.randint(1,3)
+    randomNum = random.randint(1,3)
 
-if randomNum == 1:
-    compMove = 'r'
-    print('Opponent choses Rock')
-elif randomNum == 2:
-    compMove = 'p'
-    print('Opponent selects Paper!')
-elif randomNum == 3:
-    compMove = 's'
-    print('Opponent goes with Scissors')
+    if randomNum == 1:
+        compMove = 'r'
+        print('and opponent choses Rock!')
+    elif randomNum == 2:
+        compMove = 'p'
+        print('and opponent selects Paper!')
+    elif randomNum == 3:
+        compMove = 's'
+        print('and opponent goes with Scissors!')
+
+    if userMove == 'r' and compMove == 'r':
+        print("so its a tie!")
+        ties += 1
+    elif userMove == 'r' and compMove == 'p':
+        print("so you lose!")
+        loses += 1
+    elif userMove == 'r' and compMove == 's':
+        print("so you win!")
+        wins += 1
+    elif userMove == 'p' and compMove == 'r':
+        print("so you win!")
+        wins += 1
+    elif userMove == 'p' and compMove == 'p':
+        print("so its a tie!")
+        ties += 1
+    elif userMove == 'p' and compMove == 's':
+        print("so you lose!")
+        loses += 1
+    elif userMove == 's' and compMove == 'r':
+        print("so you lose!")
+        loses += 1
+    elif userMove == 's' and compMove == 'p':
+        print("so you win!")
+        wins += 1
+    elif userMove == 's' and compMove == 's':
+        print("so you tie!")
+        ties += 1
