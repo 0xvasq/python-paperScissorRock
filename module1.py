53658df98fc42fa6c77f87f599487dc683a10f5e
0xvasq <jorge.vasquez007@mymdc.net>
2024-04-16 23:42:43 -0400

    Start the initial code

---
+++ module1.py
@@ -0,0 +1,14 @@
+
+import random, sys
+
+wins = 0
+loses = 0
+ties = 0
+
+while True:
+    print('Lets play rock, paper, scissor')
+    print('%s wins, %s loses, %s ties' % (wins, loses, ties))
+    print ('press r, p, s')
+    userMove = input("Please enter your choice")
+    break
+
